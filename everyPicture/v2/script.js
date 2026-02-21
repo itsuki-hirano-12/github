207b14d7f9a562d948434e42cdcbe45e97acec38
Itsuki Hirano <ihirano@ucdavis.edu>
2026-02-20 17:58:00 -0800

    every picture v2.1

--- everyPicture/v2/script.js
+++ everyPicture/v2/script.js
@@ -2,11 +2,7 @@
     'use strict';
     console.log('reading js');
     const cards = document.querySelectorAll('img');
-    const container = document.querySelector('#container');
-    let xPos;
-    let yPos;
-    console.log(cards);
-    const deckArea = document.querySelector('#deckArea');
+    // console.log(cards);
     let prevCard = 'initial';
 
     window.addEventListener('load', function() {
@@ -16,40 +12,29 @@
             setTimeout( function(){
                 clickCards();
                 switchText();
-            }, 2000);    
-        }, 2000);
-
-        
-        
-
-        // container.addEventListener('mousemove', reportPos);
-
-        // function reportPos(event) {
-        //     xPos = event.clientX;
-        //     yPos = event.clientY;
-        // }
-        
-
-        // for (const eachCard of cards) {
-        //     eachCard.addEventListener('click', function(event){
-        //         eachCard.style.top = '10px';
-        //         eachCard.style.left = `${xPos}`;
-        //     })
-        // }
+            }, 700);    
+        }, 1500);
     });
 
     function clickCards(){
         for (let i=0; i<cards.length; i++) {
             cards[i].addEventListener('click', function(){
                 for (const eachCard of cards) {
-                    eachCard.className = 'flipped';
+                    if (eachCard.className !== 'set') {
+                        eachCard.className = 'flipped';
+                    }             
                 }
                 if (i !== prevCard) {
                     cards[i].className = 'inPlay';
-                    prevCard = i;      
+                    prevCard = i;
                 } else {
                     prevCard = 'initial'
                 }
+
+                setTimeout( function(){
+                    flipCard(i);
+                }, 1500);
+                
                 switchText(i);
             });
         }
@@ -58,19 +43,37 @@
 
     function dealCards(){   
         for (const eachCard of cards) {
-            eachCard.className = 'flipped';
+            eachCard.className = 'set';
         }
     }
 
     function switchText(cardID){
         const number = cardID +1;
         const textBox = document.querySelector(`#text${number}`);
+        const sections = document.querySelectorAll('section');
         const initialText = document.querySelector('#initial-text');
+
+        for (const eachSection of sections) {
+                eachSection.className = 'textOut';
+            }
         if (prevCard === 'initial') {
             initialText.className = 'textInPlay';
         } else {
-            initialText.className = 'textOut';
             textBox.className = 'textInPlay';
+        }
+    }
+
+    function flipCard(cardID) {
+        const cardNumber = cardID +1;
+
+        for (const eachCard of cards) {
+            if(eachCard.className !== 'inPlay') {
+                eachCard.src = 'images/back.jpg';
+            } else if (cardNumber < 6) {
+                eachCard.src = `images/card${cardNumber}.jpg`;
+            } else {
+                eachCard.src = `images/card${cardNumber}.png`;
+            }
         }
     }
 }());
